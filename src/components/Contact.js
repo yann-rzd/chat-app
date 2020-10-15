929a57c0d9ecd15321e72b32460c839fd75b05e8
yann-rzd <yann.rouzaud@gmail.com>
2020-10-15 12:15:05 +0200

    using state for status online offline

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,27 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Contact.css'
 
-function Contact({avatar, name, online}) {
-  return (
-    <div className="Contact">
-      <img
-        className='avatar'
-        src={avatar}
-        alt="avatar"
-      />
-      <div className="info">
-        <div>
-          <p className="name">{name}</p>
+class Contact extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      online : false
+    }
+  }
+
+  event = () => {
+    const newStatus = !this.state.online
+    this.setState({online: newStatus})
+  }
+
+  render () {
+    return (
+      <div className="Contact">
+        <img
+          className='avatar'
+          src={this.props.avatar}
+          alt="avatar"
+        />
+        <div className="info">
+          <div>
+            <p className="name">{this.props.name}</p>
+          </div>
+          <div className="status">
+          <p className={this.state.online? 'status-online' : 'status-offline'} onClick={this.event}></p>
+          <p className="status-text"> 
+            {this.state.online? 'online' : 'offline'}
+          </p>
         </div>
-        <div className="status">
-        <p className={online? 'status-online' : 'status-offline'}></p>
-        <p className="status-text"> 
-          {online? 'online' : 'offline'}
-        </p>
       </div>
     </div>
-  </div>
-  )
+    )
+  }
 }
 
 export default Contact
